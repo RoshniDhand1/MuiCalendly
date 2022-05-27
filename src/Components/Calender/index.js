1d283d0322482f7382903f30eb0f5969e7ef9951
RoshniDhand1 <roshni.weproinc@gmail.com>
2022-05-27 19:18:19 +0530

    button update

--- src/Components/Calender/index.js
+++ src/Components/Calender/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React ,{useState} from 'react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { CalendarPicker } from '@mui/x-date-pickers/CalendarPicker';
@@ -10,14 +10,30 @@ import SelectTimeZone from '../SelectTimeZone';
 
 export default function Calender() {
   const [date, setDate] = React.useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
 
+   const disableWeekends = (date)=>{
+     return date.getDay() === 0 || date.getDay() === 6;
+   }
   return (
     <>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <Grid>
-            <CalendarPicker date={date} onChange={(newDate) => setDate(newDate)} />
+        <Grid container spacing={6}>
+     <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Grid  item xs={12} md={6}> 
+            <CalendarPicker 
+            date={selectedDate}
+             onChange={handleDateChange}
+             disablePast
+             shouldDisableDate={disableWeekends}
+             views={['day']}
+             
+            />
           </Grid>
         </LocalizationProvider>
+        </Grid>
     <   SelectTimeZone />
     </>
   );
--- src/Containers/index.jsx
+++ src/Containers/index.jsx
@@ -1,10 +1,11 @@
 import { Button, Grid, Paper, Typography} from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
-import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
 import { blue } from '@mui/material/colors';
 import Calender from '../Components/Calender';
+
 
 export default function Containers() {
   return (
@@ -12,7 +13,7 @@ export default function Containers() {
         <Grid container>
             <Grid item xs={4}>
                 <Box sx={{p:5}}>
-                    <a href='#'><ArrowCircleLeftOutlinedIcon  sx={{ '&:hover': { color: 'blue' ,fontSize:'450'} }}/></a> <br/>
+                    <a href='#'><ArrowBackIcon  sx={{border:1,borderRadius: '50%',fontSize:30 ,padding:0.5,borderColor: 'grey.300' ,color:'#2161ff', marginBottom:3, '&:hover': { bgcolor: '#e3f2fd' } }}/></a> 
                     <Typography fontWeight="bold" sx={{color:'#595959',fontStyle: 'normal'}}>
                         Benefit Specialists
                     </Typography>
@@ -32,12 +33,12 @@ export default function Containers() {
                 <Box sx={{p:5}}>
                     <Grid container>
                         <Grid item xs={7}>
-                        <Typography variant="h6"  fontWeight="bold"  sx={{ fontStyle: 'normal' }}>
-                            Select a Date & Time
-                        </Typography>
-                        
-                            <Calender />
-                            
+                            <Typography variant="h6"  fontWeight="bold"  sx={{ fontStyle: 'normal' }}>
+                                Select a Date & Time
+                            </Typography>
+                            <Typography   sx={{ p:3 }}>
+                                <Calender />
+                            </Typography>
                         </Grid>
                         <Grid item xs={5}>
                             TimerView
